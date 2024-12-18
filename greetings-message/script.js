ee68a53ce2708d47ae4d924c3cd888af8caef3bc
SunilG <sc3ways@gmail.com>
2024-12-18 16:00:34 +0530

    Greeting V3

--- greetings-message/script.js
+++ greetings-message/script.js
@@ -1,14 +1,21 @@
-let date = new Date();
-let hours = date.getHours();
+function greetingApp(person = "Dear") {
+  let hours = new Date().getHours();
 
-let greetingMsg = document.querySelector("#gMessage");
+  // hours = 19;
+  let greetingMsg = document.querySelector("#gMessage");
+  if (!greetingMsg) {
+    console.error("Error: Element with id 'greetingMsg' not found.");
+  }
 
-if (hours > 5 && hours < 12) {
-  greetingMsg.innerText = "Good Morning";
-  greetingMsg.classList.add("green");
-} else if (hours > 12 && hours < 18) {
-  greetingMsg.innerText = "Good Afternoon";
-  greetingMsg.classList.add("orange");
-} else {
-  greetingMsg.innerText = "Good Night";
+  if (hours >= 5 && hours < 12) {
+    greetingMsg.innerText = `Good Morning ${person}`;
+    greetingMsg.className = "green";
+  } else if (hours >= 12 && hours < 18) {
+    greetingMsg.innerText = `Good Afternoon ${person}`;
+    greetingMsg.className = "orange";
+  } else {
+    greetingMsg.innerText = `Good Night ${person}`;
+    greetingMsg.className = "blue";
+  }
 }
+greetingApp("Sunil G");
